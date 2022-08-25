3b57e41c92ddf50deaadfbc64685884e6cb984e2
Naruto <105903862+the-yatra@users.noreply.github.com>
2022-08-26 02:04:08 +0530

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./sw.js')
-    .then((reg)=>{
-        console.log("Service worker registered",reg);
-    })
-    .catch((err)=>{
-        console.log("Service worker not registered",err);
-    })
-}
